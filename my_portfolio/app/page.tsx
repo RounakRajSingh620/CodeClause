7824e896b3478b9e0234e7f59f8d5c9baa2f4da6
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-10-18 14:33:01 +0530

    CodeClause

---
+++ my_portfolio/app/page.tsx
@@ -0,0 +1,18 @@
+
+
+export default function Home() {
+  return (
+    <div className="Home">
+
+      {/*Header*/}
+      
+      {/*Hero*/}
+      {/*About*/}
+      {/*Experience*/}
+      {/*Skills*/}
+      {/*Projects*/}
+      {/*Contact Me*/}
+    </div>
+
+  )
+}
